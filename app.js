d2e37e7a39dfc977d7cf9bf7546e6dd38d33a768
CZJoanna <jyunchen199512@gmail.com>
2021-04-25 01:01:00 +0800

    comment: update query function

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
 const app = express();
 
 app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.listen(3000,()=>{
-    console.log("Express is running on http://localhost:3000")
+app.listen(3000, () => {
+  console.log("Express is running on http://localhost:3000");
 });
 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
+app.get("/", (req, res) => {
+  res.render("index", { restaurant: restaurantList.results });
+});
+
+app.get("/restaurants/:id", (req, res) => {
+  const rId = req.params.id;
+  const selectedR = restaurantList.results.find((restaurant) => {
+    return rId === restaurant.id.toString();
+  });
+  res.render("show", { restaurant: selectedR });
+});
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const selectedRs = restaurantList.results.filter((restaurant) => {
+      return restaurant.name.toLowerCase().includes(keyword.toLowerCase().trim())
+  })
+  res.render("index", { restaurant: selectedRs, keyword: keyword });
+});
