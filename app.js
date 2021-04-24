e7062ddcf17a9ebc6d3329a96e8322278e367a91
CZJoanna <jyunchen199512@gmail.com>
2021-04-24 23:42:18 +0800

    update index.hbs & show.hbs

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
+
+app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 app.listen(3000,()=>{
     console.log("Express is running on http://localhost:3000")
+});
+
+app.get("/",(req,res)=>{
+    res.render("index")
 })
