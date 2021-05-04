09294aa382513b36eb5e9a55427ee78049bd4495
CZJoanna <jyunchen199512@gmail.com>
2021-05-05 01:49:17 +0800

    modify express routes - RESTful API

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const Rest = require("./models/rest");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+// 載入 method-override
+const methodOverride = require('method-override') 
 require("./config/mongoose");
 
 // 掛載伺服器
@@ -11,13 +13,13 @@ const app = express();
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.listen(3000, () => {
   console.log("Express is running on http://localhost:3000");
 });
-
-
 
 //首頁
 app.get("/", (req, res) => {
@@ -70,26 +72,26 @@ app.get("/search", (req, res) => {
     });
 });
 //功能: 新增一筆資料
-app.post("/restaurants",(req,res)=>{
-  const newRest = req.body
+app.post("/restaurants", (req, res) => {
+  const newRest = req.body;
   return Rest.create(newRest)
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
 });
 
 //功能: 修改特定資料
-app.post("/restaurants/:id/edit", (req, res) => {
+app.put("/restaurants/:id", (req, res) => {
   const { id } = req.params;
   const editRest = req.body;
   return Rest.findByIdAndUpdate(id, editRest)
     .then(res.redirect(`/restaurants/${id}`))
-    .catch(err=>console.log(err))
+    .catch((err) => console.log(err));
 });
 
 //功能: 刪除特定資料
-app.post("/restaurants/:id/delete", (req, res) => {
+app.delete("/restaurants/:id", (req, res) => {
   const { id } = req.params;
   return Rest.findByIdAndRemove(id)
-    .then(()=>res.redirect("/"))
-    .catch(err=>console.log(err))
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
 });
